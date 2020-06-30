c4adcd6864bc6d6c86e559406cbd8d50dd9eb989
Komal Subhra <60164715+C0772144-komalsubhra@users.noreply.github.com>
2020-06-30 19:41:12 -0400

    commit

---
+++ todo_komal_C0772144/todo_komal_C0772144/Extension+TableView.swift
@@ -0,0 +1,32 @@
+//
+//  Extension+TableView.swift
+//  todo_Abhishek_C0769778
+//
+//  Created by user166476 on 6/29/20.
+//  Copyright © 2020 Quasars. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//Extenstion to handle empty table views
+extension UITableView {
+
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .black
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = UIFont(name: "TrebuchetMS", size: 18)
+        messageLabel.sizeToFit()
+
+        self.backgroundView = messageLabel
+        self.separatorStyle = .none
+    }
+
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
+}
---
+++ todo_komal_C0772144/todo_komal_C0772144/singleTask.swift
@@ -0,0 +1,17 @@
+//
+//  SingleTask.swift
+//  todo_Abhishek_C0769778
+//
+//  Created by user166476 on 6/29/20.
+//  Copyright © 2020 Quasars. All rights reserved.
+//
+
+import UIKit
+
+class SingleTask
+{
+    
+    var todoTitle: String!
+
+}
+
